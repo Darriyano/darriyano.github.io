ddf9eccafdae53eea6a51dfc5cf675ac7d122254
darriyano <android.stepanova@gmail.com>
2024-09-08 22:16:14 +0300

    Added toggle switcher for changing between pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,25 @@ import React, {useState} from 'react';
 import './App.css';
 import MainBlack from "./pages/main-black";
 
+
 function App() {
     const [toggleSwitch, toggleSwitchState] = useState(false);
 
+    const toggledSwitch = () => {
+        toggleSwitchState(!toggleSwitch);
+    }
+
+    const ToggleSwitcher = () => {
+        return (
+            <label className="switch">
+                <input type="checkbox" onChange={toggledSwitch} checked={toggleSwitch}/>
+                <span className="slider round"></span>
+            </label>
+        )
+    }
+
     return (
-        <>
+        <> <ToggleSwitcher/>
             {!toggleSwitch ? (
                     <MainBlack/>
                 ) :
